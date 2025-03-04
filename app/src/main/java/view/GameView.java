d8dad3f9f3aab08ef6dec7dc98991c8c65aab739
abg16 <149737812+abg16@users.noreply.github.com>
2025-03-04 14:49:06 -0700

    Adding more view classes and adding the attributes

--- app/src/main/java/view/GameView.java
+++ app/src/main/java/view/GameView.java
@@ -1,5 +1,21 @@
 package view;
 
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+import presenter.GamePresenter;
+
 public class GameView {
+    private GamePresenter presenter;
+    private Thread gameThread;
+    private SurfaceHolder surfaceHolder;
+    private boolean isPlaying;
+    private Canvas canvas;
+    private HealthBar healthBar;
+    private Sprite inventory;
+    private Sprite SettingsIcon;
+    private PlayerSprite playerSprite;
+    private BackgroundImage backgroundImage;
+
 
 }
