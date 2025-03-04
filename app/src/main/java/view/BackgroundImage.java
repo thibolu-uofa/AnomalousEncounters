caed26c42975e1f4d685b07aafac2c50e300cbf9
Zara721 <zaza275@icloud.com>
2025-03-04 16:06:16 -0700

    started to make background image class

--- app/src/main/java/view/BackgroundImage.java
+++ app/src/main/java/view/BackgroundImage.java
@@ -1,4 +1,22 @@
 package view;
 
-public class BackgroundImage {
+import android.graphics.Bitmap;
+
+public class BackgroundImage extends Sprite{
+    private int direction;
+    private int speed = 150; //moving speed is 150 pixels per second
+    private int fps = 10;
+
+    BackgroundImage(Bitmap imageResource, int x, int y) {
+        super(imageResource, x, y);
+    }
+
+    public void update() {
+        int newX = getX() + (speed * direction / fps);
+        setX(newX);
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
 }
