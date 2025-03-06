db001fe368fc0b0ed1c7a096c7e56964e1ae6d5f
Zara721 <zaza275@icloud.com>
2025-03-06 15:58:17 -0700

    adding the pixeltype font

--- app/src/main/java/view/GameView.java
+++ app/src/main/java/view/GameView.java
@@ -96,7 +96,7 @@ public class GameView  extends SurfaceView implements Runnable{
                 playerSprite.update(System.currentTimeMillis());
                 playerSprite.draw(canvas);
 
-                MenuItem menuItem = new MenuItem(200, 200, 200, 200, "fart", getContext());
+                MenuItem menuItem = new MenuItem(200, 200, 200, 500, "SKILLS\nCosmic Gas", getContext());
                 menuItem.draw(canvas, paint);
             }
 
--- app/src/main/java/view/MenuText.java
+++ app/src/main/java/view/MenuText.java
@@ -2,9 +2,14 @@ package view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
+import android.util.Log;
+
+import com.example.anomalousencounters.R;
 
 public class MenuText {
     private String text;
@@ -12,6 +17,7 @@ public class MenuText {
     private int color;
     private int width;
     private int textPadding = 15;
+    private final int HEADING_DIMENSION = 3;
 
     public MenuText(String text, int fontSize, int color, int width){
         this.text = text;
@@ -22,8 +28,14 @@ public class MenuText {
     public void draw(Canvas canvas, int x, int y, Context context){
         TextPaint textPaint = new TextPaint();
         textPaint.setAntiAlias(true);
-        textPaint.setTextSize(16 * context.getResources().getDisplayMetrics().density);
+        textPaint.setTextSize(32 * HEADING_DIMENSION);
+//        Log.d("Distort by", String.valueOf(context.getResources().getDisplayMetrics().density));
         textPaint.setColor(color);
+        Typeface typeface = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            typeface = context.getResources().getFont(R.font.pixeltype);
+        }
+        textPaint.setTypeface(typeface);
 
         StaticLayout staticLayout = new StaticLayout(text, textPaint, width, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0, false);
 
