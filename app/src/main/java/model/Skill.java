9bb39a516a186ab8b4e0310a68f6e16360a738d6
Zara721 <zaza275@icloud.com>
2025-03-15 16:21:28 -0600

    add constructor for skill class

--- app/src/main/java/model/Skill.java
+++ app/src/main/java/model/Skill.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Skill {
     private String name;
     private int level;
@@ -18,9 +20,27 @@ public class Skill {
         this.name = "Entity";
         this.baseDamage = 10;
         this.atkType = AttackPattern.AttackType.DIAGONAL;
+    }
 
-
-
+    public Skill(String name, String atkPatter) {
+        this.name = name;
+        this.baseDamage = 10;
+        switch (atkPatter.toUpperCase()) {
+            case "DIAGONAL":
+                this.atkType = AttackPattern.AttackType.DIAGONAL;
+                break;
+            case "STAIGHT":
+                this.atkType = AttackPattern.AttackType.STAIGHT;
+                break;
+            case "STAR":
+                this.atkType = AttackPattern.AttackType.STAR;
+                break;
+            case "CONE":
+                this.atkType = AttackPattern.AttackType.CONE;
+                break;
+            default:
+                this.atkType = AttackPattern.AttackType.STAIGHT;
+        }
     }
     //getAffectedTiles(){}
     public int getDamage(){
