f75589797a8b05ba96ec146b9c2acc12ac2eeec5
Zara721 <zaza275@icloud.com>
2025-03-15 15:32:30 -0600

    add cone attack pattern

--- app/src/main/java/model/AttackPattern.java
+++ app/src/main/java/model/AttackPattern.java
@@ -5,7 +5,8 @@ public class AttackPattern {
     public enum AttackType {
         DIAGONAL,
         STAIGHT,
-        STAR
+        STAR,
+        CONE
     }
 
     private final AttackType attackType;
@@ -25,6 +26,8 @@ public class AttackPattern {
                 return getStraightPattern(origin_pos, distance);
             case STAR:
                 return getStarPattern(origin_pos, distance);
+            case CONE:
+                return getConePattern(origin_pos, distance);
             default:
                 return new ArrayList<int[]>();
         }
@@ -42,6 +45,11 @@ public class AttackPattern {
 
     private ArrayList<int[]> getStarPattern(int[] origin_pos, int distance) {
         int[][] positionVectors = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        return getRepeatingPattern(origin_pos, distance, positionVectors);
+    }
+
+    private ArrayList<int[]> getConePattern(int[] origin_pos, int distance) {
+        int[][] positionVectors = {{-1, 1}, {0, 1}, {1, 1}};
         return getRepeatingPattern(origin_pos, distance, positionVectors);
     }
 
