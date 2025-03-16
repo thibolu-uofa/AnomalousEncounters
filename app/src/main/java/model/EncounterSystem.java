df03e33e3a2a37b2c230e90fd278746ed7e07f05
abg16 <149737812+abg16@users.noreply.github.com>
2025-03-15 18:25:38 -0600

    Creating the EncounterSystem class where it keeps the position of the stones and gets a random enemy to battle.

---
+++ app/src/main/java/model/EncounterSystem.java
@@ -0,0 +1,43 @@
+package model;
+import static model.Utils.getListOfDataProperty;
+import java.util.ArrayList;
+import java.util.Random;
+
+import presenter.GamePresenter;
+
+public class EncounterSystem {
+    private GamePresenter presenter;
+    private final ArrayList<int[]> startAndEndPoints = new ArrayList<>();
+    private final int ENCOUNTER_PROBABILITY = 4;
+    private int[] enemyIds = {0, 1, 2};
+
+    public EncounterSystem() {
+        startAndEndPoints.add(new int[]{-927, -1172});
+        startAndEndPoints.add(new int[]{-1826, -2003});
+        startAndEndPoints.add(new int[]{-2610, -2810});
+    }
+    public boolean hasEncounteredEnemy(int x) {
+        if (canEncounterEnemy(x)) {
+            Random rand = new Random();
+            int diceThrow = rand.nextInt(6) + 1;
+            return ENCOUNTER_PROBABILITY >= diceThrow;
+        }
+        return false;
+    }
+
+    private boolean canEncounterEnemy(int x) {
+        for (int[] startAndEndPoint: startAndEndPoints){
+            if (x >= startAndEndPoint[0] && x <= startAndEndPoint[1]){
+               return true;
+            }
+        }
+        return false;
+    }
+
+    public int getRandomEnemyId() {
+        Random rand = new Random();
+        int randomEnemyId = rand.nextInt(enemyIds.length);
+        return enemyIds[randomEnemyId];
+    }
+
+}
