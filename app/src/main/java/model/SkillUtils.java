b17234f1814899b617291844ec7787e35b53d09b
Siletbanshee <danielawogu@gmail.com>
2025-03-27 13:10:45 -0600

    Wrote getUpdatedLevelAndExperience and getMaxExperience in SkillUtils.java

--- app/src/main/java/model/SkillUtils.java
+++ app/src/main/java/model/SkillUtils.java
@@ -1,17 +1,23 @@
 package model;
 
 public class SkillUtils {
-    public static int[] getUpdatedLevelAndExperience(int level, int currentExperience) {
+    public static int[] getUpdatedLevelAndExperience(int level, int currentExperience, int experienceGiven) {
         //this functions increases skill experience (use some math, can just increase experience by 10 for now),
         // then add the calculated experience to currentExperience, if greater than maxExperience (which you get from getMaxExperience)
         // find out the extra amount and setCurrent experience to the extra amount and increase skill level by one
         // return the updated currentExperience and updated level
-
+        int maxExperience = getMaxExperience(level);
+        currentExperience += experienceGiven;
+        if (currentExperience >= maxExperience){
+            currentExperience = currentExperience - maxExperience;
+            level++;
+        }
         return new int[]{level, currentExperience};
     }
 
-    private int getMaxExperience(int level) {
+    private static int getMaxExperience(int level) {
         //return max experience based on level, for example level times 100
-        return -1;
+
+        return (2 ^ level) +  100;
     }
 }
