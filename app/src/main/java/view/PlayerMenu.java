27012c3241bbbfe8b325604338a9942ac860fa63
Zara721 <zaza275@icloud.com>
2025-03-30 13:02:39 -0600

    refactored player menu to use helper functions to draw things for better readability

--- app/src/main/java/view/PlayerMenu.java
+++ app/src/main/java/view/PlayerMenu.java
@@ -1,22 +1,25 @@
 package view;
 
-import static view.ViewConstants.OVERLAY_DARK_COLOR;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Objects;
 import presenter.GamePresenter;
 
 public class PlayerMenu extends BaseMenu{
     private RadioBtnList skillRadioBtnList;
     private RadioBtnList itemRadioBtnList;
-    String selectedItem;
-    String selectedSkill;
+    private String selectedItem;
+    private String selectedSkill;
+    private final int Y = 200;
+    private final float LINE_SPACE_MULTIPLIER = 1.2f;
+    private final int MENU_HEIGHT = 500;
+    private final int MENU_WIDTH = 700;
+    private final int COL_WIDTH = 100;
+    private final int RADIO_X_PAD = 15;
+    private final int RADIO_Y_PAD = 90;
 
     /**
      * Creates a player menu interface with various menu items.
@@ -27,35 +30,40 @@ public class PlayerMenu extends BaseMenu{
     public PlayerMenu(GamePresenter presenter, Context context){
         super(presenter, context);
         int x = 100;
-        int Y = 200;
         int MARGIN = 50;
-        float LINE_SPACE_MULTIPLIER = 1.2f;
-        int MENU_HEIGHT = 500;
-        int MENU_WIDTH = 700;
-        int COL_WIDTH = 100;
-        int RADIO_X_PAD = 15;
-        int RADIO_Y_PAD = 90;
-
-        String SKILL_HEADING = "SKILLS";
-        String LEVEL_HEADING = "LV";
-        String ITEM_HEADING = "ITEMS";
-        String ITEM_AMOUNT_HEADING = "#";
 
         createPlayerCard(new Rectangle(x, Y), context);
         x += PLAYER_CARD_WIDTH + MARGIN;
 
+        createSkillMenu(x);
+        x += MENU_WIDTH + COL_WIDTH + MARGIN;
+
+        createItemMenu(x);
+    }
+
+    private void createSkillMenu(int x) {
+        String SKILL_HEADING = "SKILLS";
         MenuItem skills = new MenuItem(x, Y, MENU_HEIGHT, MENU_WIDTH, SKILL_HEADING, false, context);
         menuItemsList.put("skills", skills);
 
         ArrayList<String> skillArrayList = presenter.getSkillNamesArray();
         Rectangle skillListRect = new Rectangle(x + RADIO_X_PAD, Y + RADIO_Y_PAD);
         skillRadioBtnList = new RadioBtnList(skillListRect.x, skillListRect.y, context, skillArrayList, MENU_WIDTH, MENU_HEIGHT);
-        x += skills.getWidth();
+
+        x += MENU_WIDTH;
+        createLevelColumn(x);
+    }
+
+    private void createLevelColumn(int x) {
+        String LEVEL_HEADING = "LV";
 
         MenuItem level = new MenuItem(x, Y, MENU_HEIGHT, COL_WIDTH, LEVEL_HEADING, true, context);
         level.setLineSpacingMultiplier(LINE_SPACE_MULTIPLIER);
         menuItemsList.put("skill_levels", level);
-        x += level.getWidth() + MARGIN;
+    }
+
+    private void createItemMenu(int x) {
+        String ITEM_HEADING = "ITEMS";
 
         MenuItem items = new MenuItem(x, Y, MENU_HEIGHT, MENU_WIDTH, ITEM_HEADING, false, context);
         menuItemsList.put("items", items);
@@ -63,14 +71,17 @@ public class PlayerMenu extends BaseMenu{
         ArrayList<String> itemArrayList = presenter.getPlayerItemNamesArray();
         Rectangle itemListRect = new Rectangle(x + RADIO_X_PAD, Y + RADIO_Y_PAD);
         itemRadioBtnList = new RadioBtnList(itemListRect.x, itemListRect.y, context, itemArrayList, MENU_WIDTH, MENU_HEIGHT);
+
         x += items.getWidth();
+        createItemAmountColumn(x);
+    }
+
+    private void createItemAmountColumn(int x) {
+        String ITEM_AMOUNT_HEADING = "#";
 
         MenuItem amountOfItems = new MenuItem(x, Y, MENU_HEIGHT, COL_WIDTH, ITEM_AMOUNT_HEADING, true, context);
         amountOfItems.setLineSpacingMultiplier(LINE_SPACE_MULTIPLIER);
         menuItemsList.put("item_amounts", amountOfItems);
-
-        //MenuItem infoButton = new MenuItem(400, INITIAL_Y, 500, 400, "INFO", true, context);
-        //menuItemsList.add(infoButton);
     }
 
     public void updateMenuTexts(GamePresenter presenter) {
